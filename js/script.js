0c70fd6d3838c25d1084ccd85f756a0227f9456a
eleda <elekesda@gmail.com>
2016-12-31 16:23:29 +0100

    minden

---
+++ js/script.js
@@ -0,0 +1,13 @@
+		function funnyText() {
+			$('.ynum h1').funnyText({
+				speed: 200,
+				fontSize: '1em',
+				color: '#191919',
+				activeColor: '#eee',
+				borderColor: 'black'
+			});
+		}
+
+		$(document).ready(function() {
+			funnyText();
+		});
